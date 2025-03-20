0113574acc3454c2b0e53ecf9a024a3f9def345d
MENG Vuthvitou <liltou081@gmail.com>
2025-03-20 23:53:24 +0700

    Create userSchema

---
+++ models/User.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const bcryptjs = require("bcryptjs");
+
+// create schema
+const UserSchema = new mongoose.Schema(
+  {
+    fullName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    profileUrl: { type: String, default: null },
+  },
+  { timestamps: true }
+);
+
+// hash password
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcryptjs.hash(this.password, 10);
+
+  next();
+});
+
+// compare password
+UserSchema.methods.comparePassword = async (pass) =>
+  await bcryptjs.compare(pass, this.password);
+
+module.exports = mongoose.model("User", UserSchema);
