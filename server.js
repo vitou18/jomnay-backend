dc5e6011bf51938f25c2e4629fbddad480824544
MENG Vuthvitou <liltou081@gmail.com>
2025-03-20 23:41:43 +0700

    Connect database

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
-console.log("Hello");
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const connectDb = require("./config/db.js");
+
+// Connect db
+connectDb();
+
+const app = express();
+
+// Middleware
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
